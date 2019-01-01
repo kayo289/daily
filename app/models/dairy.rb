1756563d500bf954dddfb24a50f4861f85179baf
kayo289 <kayokapi@icloud.com>
2019-01-01 12:19:10 +0000

    [new]validation

--- app/models/dairy.rb
+++ app/models/dairy.rb
@@ -1,4 +1,18 @@
 class Dairy < ApplicationRecord
 	validates :title, presence: true
 	validates :body, presence: true
+    validates :proper_title_and_body
+
+   private
+
+    def proper_title_and_body
+      unless title.starts_with?('今日')
+        errors.add(:title, 'は「今日」は始めてください。')
+      end
+
+      unless body.end_with?('。')
+        errors.add(:body,'は句点(。)で終了してください。')
+      end
+    end
+
 end
